b6737abc9c8c0fd07bbae1479bcdb3eaee5d93a7
Matheus <Matheuscardozoesilva@gmail.com>
2024-10-04 23:59:50 -0300

    ApiWeb

--- lib/src/modules/home/service/home_service.dart
+++ lib/src/modules/home/service/home_service.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;  // Importando http
 import 'package:fast_location/src/modules/home/model/address_model.dart';
+
+import '../model/address_model.dart';
 
 class HomeService {
   // Simulação de um banco de dados ou API
@@ -21,18 +25,28 @@ class HomeService {
     },
   ];
 
-  // Método para obter a lista de endereços
   Future<List<AddressModel>> getAddressHistoryList() async {
     // Simula um atraso de rede
     await Future.delayed(Duration(seconds: 2));
-    return _addressHistoryData.map((data) => AddressModel.fromJsonLocal(data)).toList();
+    return _addressHistoryData.map((data) => AddressModel.fromJson(data)).toList();
   }
 
   // Método para obter um endereço pelo CEP
   Future<AddressModel> getAddressByCep(String cep) async {
-    // Simula um atraso de rede
-    await Future.delayed(Duration(seconds: 2));
-    final data = _addressHistoryData.firstWhere((element) => element['cep'] == cep, orElse: () => throw Exception('CEP não encontrado'));
-    return AddressModel.fromJsonLocal(data);
+    // Remove caracteres não numéricos do CEP
+    final cleanedCep = cep.replaceAll(RegExp(r'[^0-9]'), '');
+
+    final url = 'https://viacep.com.br/ws/$cleanedCep/json/';
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      if (data['erro'] != null) {
+        throw Exception('CEP não encontrado');
+      }
+      return AddressModel.fromJson(data);
+    } else {
+      throw Exception('Erro ao buscar endereço');
+    }
   }
 }
